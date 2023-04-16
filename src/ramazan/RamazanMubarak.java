68ed5c98b5d8c72cb3f7bb9f729aeeb6f6540e17
mohammadpanahnikzad <mohammadpanahnikzad@localhost>
2023-04-15 22:39:34 -0400

    Month of Forgivness..

--- src/ramazan/RamazanMubarak.java
+++ src/ramazan/RamazanMubarak.java
@@ -5,6 +5,8 @@ public class RamazanMubarak {
 	public static void main(String args []) {
 		
 		System.out.println("Ramazan is about to finish....");
+		
+		System.out.println("Ramazan is month of Forgivness...");
 	}
 
 }
